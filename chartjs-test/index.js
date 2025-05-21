9fbe37cddf7935441a006b68dca0c536be6446db
JoaoPauloFerrariSatAna <joaopauloferrarisantana@gmail.com>
2025-05-20 21:33:29 -0300

    feat: added another test

---
+++ chartjs-test/index.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("toDraw");
+
+canvas.addEventListener("load", showChart());
+
+function showChart() {
+  new Chart(canvas, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
