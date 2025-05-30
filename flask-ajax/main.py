5a282d798bd8a03030ed4032943ce2bf723d370c
JoaoPauloFerrariSantAna <94869217+JoaoPauloFerrariSantAna@users.noreply.github.com>
2025-05-29 22:14:39 -0300

    updated test

--- flask-ajax/main.py
+++ flask-ajax/main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return render_template("index.html", banana="maca")
+    return render_template("index.html")
 
 @app.get("/teste")
 def getTeste():
-    return jsonify({"word":"banana"})
+    req = request.args.get("fruta")
+
+    print(req)
+
+    return jsonify({"returnedAction": req})
